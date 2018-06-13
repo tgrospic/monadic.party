425e97cb78149df75719d4fefc809bb302716967
Tomislav Grospic <grospic@gmail.com>
2018-06-13 12:31:50 +0200

    stm sample

--- stm/stm-sample/app/Main.hs
+++ stm/stm-sample/app/Main.hs
@@ -3,21 +3,27 @@ module Main where
 import Lib
 import Control.Concurrent.STM
 import Data.Decimal
+import Data.Maybe
 import Control.Arrow
 import Control.Monad
 import Data.Bifunctor
+import System.Random
 
 main :: IO ()
-main = sample1 *> sample2
+main = sample1 *> sample2 *> randomDecimalPrint
 
-account1 = newTVar 50.0
-account2 = newTVar 100.0
+account1, account2, bank :: STM (TVar Integer)
+account1 = newTVar 50
+account2 = newTVar 100
+bank     = newTVar 100000
 
 sample1 = do
   a1 <- atomically account1
   a2 <- atomically account2
+  val <- (randomIO :: IO Integer)
+  print ("Random", val)
   -- Do transfer
-  atomically $ transfer a1 a2 9
+  atomically $ transfer a1 a2 val
   -- Get values
   a1' <- readTVarIO a1
   a2' <- readTVarIO a2
@@ -26,11 +32,17 @@ sample1 = do
 
 sample2 = atomically tr >>= print
   where
-    tr = join $ transferWithResult 9 <$> account1 <*> account2
+    tr = join $ transferWithResult 49 <$> account1 <*> account2
     -- Transfer
     transferWithResult v a1 a2 = do
-      transfer a1 a2 v
+      transfer2 a1 a2 v
       x1 <- readTVar a1
       x2 <- readTVar a2
       pure (x1, x2)
 
+-- randomInt max = do
+--   g <- getStdRandom (randomR (1,6))
+
+randomDecimalPrint = do
+  g <- getStdGen
+  print $ take 2 (randoms g :: [Double])
--- stm/stm-sample/src/Lib.hs
+++ stm/stm-sample/src/Lib.hs
@@ -1,34 +1,48 @@
 module Lib
     ( transfer
+    , transfer2
     , debit
     , credit
     ) where
 
 import Control.Concurrent.STM
 import Data.Decimal
+import Control.Concurrent.Async
 
-type Account = TVar Decimal
+type Account = TVar Integer
 
-debit :: Account -> Decimal -> STM ()
+debit :: Account -> Integer -> STM ()
 debit acc v = do
   s <- readTVar acc
-  writeTVar acc (s - v)
-  pure ()
+  if s >= v
+    then writeTVar acc (s - v)
+    else retry
 
-credit :: Account -> Decimal -> STM ()
+credit :: Account -> Integer -> STM ()
 credit acc v = do
   s <- readTVar acc
   writeTVar acc (s + v)
-  pure ()
 
-transfer :: Account -> Account -> Decimal -> STM ()
+transfer :: Account -> Account -> Integer -> STM ()
 transfer from to amount = do
   debit  from amount
   credit to   amount
 
-transfer2 :: Account -> Account -> Decimal -> STM ()
-transfer2 from to amount = orElse actualTransfer (pure ())
+transfer2 :: Account -> Account -> Integer -> STM ()
+transfer2 from to amount = actualTransfer `orElse` pure ()
   where
     actualTransfer = do
       debit  from amount
       credit to   amount
+
+{- 
+3. Are our invariants really invariant?
+Create a third bank account, to serve as our "bank". Give it an insane amount of money.
+
+Add a dependency on the async and random packages. Import Control.Concurrent.Async and try to find whatever you need from the random package.
+
+Create a 5 threads using race_ (excersise in itself). Spawn the following function:
+-}
+
+thread :: Int -> [Account] -> IO ()
+thread numTransactions accounts = undefined
